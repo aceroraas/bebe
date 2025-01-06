b8a556ed53e2a0995bbbb81d857b766daeb60c07
RAAS <acerocol67@gmail.com>
2025-01-06 03:32:22 -0400

    avances

--- src/shared/hooks/useConfetti.js
+++ src/shared/hooks/useConfetti.js
@@ -1,48 +1,16 @@
-import confetti from 'canvas-confetti';
+import JSConfetti from 'js-confetti';
 
 export function useConfetti() {
+   const jsConfetti = new JSConfetti();
+
    const launchConfetti = ({ type = 'girl' } = {}) => {
-      const count = 200;
-      const defaults = {
-         origin: { y: 0.7 },
-         colors: type === 'boy' 
-            ? ['#1e40af', '#3b82f6', '#93c5fd', '#dbeafe', '#ffffff'] // Azules y blanco para niño
-            : ['#be185d', '#ec4899', '#f9a8d4', '#fce7f3', '#ffffff']  // Rosas y blanco para niña
-      };
+      const colors = type === 'boy' 
+         ? ['#1e40af', '#3b82f6', '#93c5fd', '#dbeafe', '#ffffff'] // Azules y blanco para niño
+         : ['#be185d', '#ec4899', '#f9a8d4', '#fce7f3', '#ffffff'];  // Rosas y blanco para niña
 
-      function fire(particleRatio, opts) {
-         confetti({
-            ...defaults,
-            ...opts,
-            particleCount: Math.floor(count * particleRatio)
-         });
-      }
-
-      fire(0.25, {
-         spread: 26,
-         startVelocity: 55,
-      });
-
-      fire(0.2, {
-         spread: 60,
-      });
-
-      fire(0.35, {
-         spread: 100,
-         decay: 0.91,
-         scalar: 0.8
-      });
-
-      fire(0.1, {
-         spread: 120,
-         startVelocity: 25,
-         decay: 0.92,
-         scalar: 1.2
-      });
-
-      fire(0.1, {
-         spread: 120,
-         startVelocity: 45,
+      jsConfetti.addConfetti({
+         confettiColors: colors,
+         confettiNumber: 200,
       });
    };
 
